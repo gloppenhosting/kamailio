9f5b45043e8a4aab9e7a2d21eee058e63f5a66a4
Andreas Kruger <ak@krugercorp.dk>
2015-09-21 14:07:32 +0200

    Validate kamailio config

--- run.sh
+++ run.sh
@@ -14,5 +14,8 @@ service rsyslog start
 kama_shr=${KAMAILIO_SHR-64}
 kama_pkg=${KAMAILIO_PKG-24}
 
+# Test the syntax.
+kamailio -c
+
 # And get kama going.
 kamailio -M ${kama_pkg} -m ${kama_shr} -DD -E -e
